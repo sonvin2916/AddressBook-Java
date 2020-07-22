bd621a9376edba2c9dded44d296dc8a2ac3565ec
sonvin2916 <sonalchougule07@gmail.com>
2020-07-22 15:08:35 +0530

    [Sonal]Added : Multiple person added to addressbook

--- AddressBook/src/main/java/Person.java
+++ AddressBook/src/main/java/Person.java
@@ -1,10 +1,13 @@
 
+
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.*;
 
 public class Person
 {
     Scanner scanner=new Scanner(System.in);
+    ArrayList<Person> arrayList = new ArrayList<Person>();
     private String firstName;
     private String lastName;
     private String address;
@@ -12,7 +15,7 @@ public class Person
     private String state;
     private int zip;
     private int phoneNumber;
-    Person(){}
+    Person person;
     Person(String firstName, String lastName, String address, String city, String state, int zip, int phoneNumber)
     {
         this.firstName = firstName;
@@ -79,84 +82,136 @@ public class Person
     public void setPhoneNumber(int phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
-
-    public void display()
+    void addPerson()
     {
-        System.out.println("\nAddress Book\n FirstName:" + firstName + "\nLastName:" + lastName + "\nAddress:" + address + "\nState:" + state + "\nCity:" + city + "\nZip:" + zip + "\nPhone:" + phoneNumber);
-    }
-
-    public static void main(String[] args)
-    {
-
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Person> arrayList = new ArrayList<Person>();
-        System.out.println("Enter First Name");
-        String firstName = scanner.next();
-
-        System.out.println("Enter Last Name");
-        String lastName = scanner.next();
-
-        System.out.println("Enter Address");
-        String address = scanner.next();
-
-        System.out.println("Enter City");
-        String city = scanner.next();
-
-        System.out.println("Enter State");
-        String state = scanner.next();
-
-        System.out.println("Enter Zip Code");
-        int zip = scanner.nextInt();
-
-        System.out.println("Enter Phone Number");
-        int phoneNumber = scanner.nextInt();
-
-        Person person = new Person(firstName, lastName, address, city, state, zip, phoneNumber);
-        person.display();
-
-        System.out.println("What do you want to edit? \n1.Address \n2.City \n3.State \n4.Zip \n5.PhoneNumber");
-        int choice = scanner.nextInt();
-        switch (choice)
+        System.out.println("Enter the total number of people you want to add");
+        int number = scanner.nextInt();
+        for(int i=0; i<number; i++)
         {
-            case 1 : System.out.println("Enter Address");
-                     address = scanner.next();
-                     person.setAddress(address);
-                     break;
-            case 2 : System.out.println("Enter City");
-                      city = scanner.next();
-                     person.setCity(city);
-                     break;
-            case 3 : System.out.println("Enter State");
-                     state = scanner.next();
-                     person.setState(state);
-                     break;
-            case 4 : System.out.println("Enter Zip");
-                     zip = scanner.nextInt();
-                     person.setZip(zip);
-                     break;
-            case 5 : System.out.println("Enter Phone Number");
-                     phoneNumber =  scanner.nextInt();
-                     person.setPhoneNumber(phoneNumber);
-                     break;
+            System.out.println("Enter First Name");
+            String firstName = scanner.next();
+
+            System.out.println("Enter Last Name");
+            String lastName = scanner.next();
+
+            System.out.println("Enter Address");
+            String address = scanner.next();
+
+            System.out.println("Enter City");
+            String city = scanner.next();
+
+            System.out.println("Enter State");
+            String state = scanner.next();
+
+            System.out.println("Enter Zip Code");
+            int zip = scanner.nextInt();
+
+            System.out.println("Enter Phone Number");
+            int phoneNumber = scanner.nextInt();
         }
-        arrayList.add(new Person(firstName, lastName, address, city, state, zip, phoneNumber));
-        person.display();
-
-        System.out.println("Enter the First Name of the person ");
-        String name = scanner.next();
-        for(Person deletePerson:arrayList)
+        for(Person add : arrayList)
         {
-            if(arrayList.contains(name))
-            {
-                arrayList.remove(deletePerson);
-                System.out.println("Deleted Successfully");
+            System.out.println("First Name :"+add.getFirstName()+"\nLast Name:"+add.getLastName()+"\nAddress:"+add.getAddress()+"\nState:"+add.getState()+"\nCity:"+add.getCity()+"\nZip:"+add.getZip()+"\nPhone Number:"+add.getPhoneNumber());
+        }
+
+    }
+    void editPerson() {
+        System.out.println("Enter Person's First Name");
+        String firsName = scanner.next();
+        for (int index = 0; index < arrayList.size(); index++) {
+            person = (Person) arrayList.get(index);
+            if (firsName.equals(person.firstName)) {
+                System.out.println("Record found" + person.firstName);
+                System.out.println("What do you want to edit? \n1.Address \n2.City \n3.State \n4.Zip \n5.PhoneNumber");
+                int choice = scanner.nextInt();
+                switch (choice) {
+                    case 1:
+                        System.out.println("Enter Address");
+                        address = scanner.next();
+                        person.setAddress(address);
+                        break;
+                    case 2:
+                        System.out.println("Enter City");
+                        city = scanner.next();
+                        person.setCity(city);
+                        break;
+                    case 3:
+                        System.out.println("Enter State");
+                        state = scanner.next();
+                        person.setState(state);
+                        break;
+                    case 4:
+                        System.out.println("Enter Zip");
+                        zip = scanner.nextInt();
+                        person.setZip(zip);
+                        break;
+                    case 5:
+                        System.out.println("Enter Phone Number");
+                        phoneNumber = scanner.nextInt();
+                        person.setPhoneNumber(phoneNumber);
+                        break;
+                    default:
+                        System.out.println("Invalid Choice");
+                        break;
+                }
+                System.out.print("First Name :" + person.getFirstName() + "\nLast Name:" + person.getLastName() + "\nAddress:" + person.getAddress() + "\nState:" + person.getState() + "\nCity:" + person.getCity() + "\nZip:" + person.getZip() + "\nPhone Number:" + person.getPhoneNumber());
+            } else {
+                System.out.println("No Matching record found");
             }
         }
-
-
+    }
+    void deletePerson()
+    {
+        System.out.println("Enter Person's First Name");
+        String firstName=scanner.next();
+        for(int index=0; index<arrayList.size();index++)
+        {
+            person=(Person)arrayList.get(index);
+            if(firstName.equals(person.firstName))
+            {
+                System.out.println("Record Found"+person.firstName);
+                System.out.println("Are you sure you want to delete \n1.Yes \n2.No ");
+                int choice = scanner.nextInt();
+                if(choice==1)
+                {
+                    arrayList.remove(index);
+                }
+                else
+                {
+                    System.out.println("Not Deleted");
+                }
+            }
+        }
     }
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Address Book Application");
+        String firstName = null, lastName = null, address = null, city = null, state = null;
+        int zip = 0, phoneNumber = 0;
+        Person personDetails = new Person(firstName, lastName, address, city, state, zip, phoneNumber);
+        while (true) {
+            System.out.println("Enter your choice \n1.Add a Person \n2.Edit a Person \n3.Delete a Person \n4.Exit");
+            int select = scanner.nextInt();
+            switch (select) {
+                case 1:
+                    personDetails.addPerson();
+                    ;
+                    break;
+                case 2:
+                    personDetails.editPerson();
+                    break;
+                case 3:
+                    personDetails.deletePerson();
+                    break;
+                default:
+                    System.out.println("Exit");
+                    break;
 
+
+            }
+        }
+    }
 
 }
 
